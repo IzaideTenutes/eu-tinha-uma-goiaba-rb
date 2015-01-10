0c83d2031be5ae12dd8e0d9ed410a15bdc0d1cb2
DadoCe <dado.ce@gmail.com>
2015-01-10 13:05:39 -0300

    Rename refactoring

---
+++ lib/decisor.rb
@@ -0,0 +1,32 @@
+require 'CompraGoiaba'
+require 'CompraPassagem'
+
+class Decisor
+  attr_accessor :dinheiro_na_carteira, :estar_faminto
+
+  def initialize(dinheiro_na_carteira, estar_faminto)
+    self.dinheiro_na_carteira = dinheiro_na_carteira
+    self.estar_faminto = estar_faminto
+  end
+
+  def decide
+    decisao = []
+    opcoes = [
+      CompraGoiaba.new,
+      CompraPassagem.new
+    ]
+
+    opcoes.each do | opcao |
+      if opcao.pode_fazer? dinheiro_na_carteira, estar_faminto
+        paga opcao.valor 
+        decisao << opcao.descricao
+      end
+    end
+
+    decisao.join(" e ")
+  end
+
+  def paga(valor)
+    self.dinheiro_na_carteira -= valor
+  end
+end
