d32e17f0cc5de15c5e8339c0acbf47784385624c
DadoCe <dado.ce@gmail.com>
2015-01-10 15:47:50 -0300

    Refactoring: DI

--- lib/decisor.rb
+++ lib/decisor.rb
@@ -1,22 +1,19 @@
-require 'CompraGoiaba'
-require 'CompraPassagem'
+require 'compra_goiaba'
+require 'compra_passagem'
 
 class Decisor
   attr_accessor :dinheiro_na_carteira, :estar_faminto
 
-  def initialize(dinheiro_na_carteira, estar_faminto)
+  def initialize(dinheiro_na_carteira, estar_faminto, opcoes)
     self.dinheiro_na_carteira = dinheiro_na_carteira
     self.estar_faminto = estar_faminto
+    @opcoes = opcoes
   end
 
   def decide
     decisao = []
-    opcoes = [
-      CompraGoiaba.new,
-      CompraPassagem.new
-    ]
 
-    opcoes.each do | opcao |
+    @opcoes.each do | opcao |
       if opcao.pode_fazer? dinheiro_na_carteira, estar_faminto
         paga opcao.valor 
         decisao << opcao.descricao
